a64b328d6f766745365135c2d61c36e8c8c9f86b
mensinda <daniel@mensinger-ka.de>
2017-06-02 15:53:06 +0200

    Switched to FindGLIB2.cmake

--- FindPCAP.cmake
+++ FindPCAP.cmake
@@ -1,74 +1,97 @@
-# - Try to find libpcap include dirs and libraries
 #
-# Usage of this module as follows:
+# - Find pcap and winpcap
+# Find the native PCAP includes and library
 #
-#     find_package(PCAP)
-#
-# Variables used by this module, they can change the default behaviour and need
-# to be set before calling find_package:
-#
-#  PCAP_ROOT_DIR             Set this variable to the root installation of
-#                            libpcap if the module has problems finding the
-#                            proper installation path.
-#
-# Variables defined by this module:
-#
-#  PCAP_FOUND                System has libpcap, include and library dirs found
-#  PCAP_INCLUDE_DIR          The libpcap include directories.
-#  PCAP_LIBRARY              The libpcap library (possibly includes a thread
-#                            library e.g. required by pf_ring's libpcap)
-#  HAVE_PF_RING              If a found version of libpcap supports PF_RING
+#  PCAP_INCLUDE_DIRS - where to find pcap.h, etc.
+#  PCAP_LIBRARIES    - List of libraries when using pcap.
+#  PCAP_FOUND        - True if pcap found.
 
-find_path(PCAP_ROOT_DIR
-    NAMES include/pcap.h
+include( FindWSWinLibs )
+FindWSWinLibs( "WpdPack" "PCAP_HINTS" )
+
+# The 64-bit wpcap.lib is under /x64
+set ( _PLATFORM_SUBDIR "" )
+if( WIN32 AND "${WIRESHARK_TARGET_PLATFORM}" STREQUAL "win64" )
+  set ( _PLATFORM_SUBDIR "/x64" )
+endif()
+
+find_path( PCAP_INCLUDE_DIR
+  NAMES
+  pcap/pcap.h
+  pcap.h
+  HINTS
+    "${PCAP_HINTS}/include"
 )
 
-find_path(PCAP_INCLUDE_DIR
-    NAMES pcap.h
-    HINTS ${PCAP_ROOT_DIR}/include
+find_library( PCAP_LIBRARY
+  NAMES
+    pcap
+    wpcap
+  HINTS
+    "${PCAP_HINTS}/lib${_PLATFORM_SUBDIR}"
 )
 
-find_library(PCAP_LIBRARY
-    NAMES pcap
-    HINTS ${PCAP_ROOT_DIR}/lib
-)
 
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(PCAP DEFAULT_MSG
-    PCAP_LIBRARY
-    PCAP_INCLUDE_DIR
-)
+include( FindPackageHandleStandardArgs )
+find_package_handle_standard_args( PCAP DEFAULT_MSG PCAP_INCLUDE_DIR PCAP_LIBRARY )
 
-include(CheckCSourceCompiles)
-set(CMAKE_REQUIRED_LIBRARIES ${PCAP_LIBRARY})
-check_c_source_compiles("int main() { return 0; }" PCAP_LINKS_SOLO)
-set(CMAKE_REQUIRED_LIBRARIES)
+if( PCAP_FOUND )
+  set( PCAP_INCLUDE_DIRS ${PCAP_INCLUDE_DIR} )
+  set( PCAP_LIBRARIES ${PCAP_LIBRARY} )
+else()
+  set( PCAP_INCLUDE_DIRS )
+  set( PCAP_LIBRARIES )
+endif()
 
-# check if linking against libpcap also needs to link against a thread library
-if (NOT PCAP_LINKS_SOLO)
-    find_package(Threads)
-    if (THREADS_FOUND)
-        set(CMAKE_REQUIRED_LIBRARIES ${PCAP_LIBRARY} ${CMAKE_THREAD_LIBS_INIT})
-        check_c_source_compiles("int main() { return 0; }" PCAP_NEEDS_THREADS)
-        set(CMAKE_REQUIRED_LIBRARIES)
-    endif ()
-    if (THREADS_FOUND AND PCAP_NEEDS_THREADS)
-        set(_tmp ${PCAP_LIBRARY} ${CMAKE_THREAD_LIBS_INIT})
-        list(REMOVE_DUPLICATES _tmp)
-        set(PCAP_LIBRARY ${_tmp}
-            CACHE STRING "Libraries needed to link against libpcap" FORCE)
-    else ()
-        message(FATAL_ERROR "Couldn't determine how to link against libpcap")
-    endif ()
-endif ()
+if( PCAP_FOUND )
+  #Functions
+  include( CMakePushCheckState )
+  include( CheckFunctionExists )
+  include( CheckVariableExists )
 
-include(CheckFunctionExists)
-set(CMAKE_REQUIRED_LIBRARIES ${PCAP_LIBRARY})
-check_function_exists(pcap_get_pfring_id HAVE_PF_RING)
-set(CMAKE_REQUIRED_LIBRARIES)
+  cmake_push_check_state()
+  set( CMAKE_REQUIRED_INCLUDES ${PCAP_INCLUDE_DIRS} )
+  set( CMAKE_REQUIRED_LIBRARIES ${PCAP_LIBRARIES} )
 
-mark_as_advanced(
-    PCAP_ROOT_DIR
-    PCAP_INCLUDE_DIR
-    PCAP_LIBRARY
-)
+  check_function_exists( "pcap_open_dead" HAVE_PCAP_OPEN_DEAD )
+  check_function_exists( "pcap_freecode" HAVE_PCAP_FREECODE )
+  #
+  # Note: for pcap_breakloop() and pcap_findalldevs(), the autoconf script
+  # checks for more than just whether the function exists, it also checks
+  # for whether pcap.h declares it; macOS software/security updates can
+  # update libpcap without updating the headers.
+  #
+  check_function_exists( "pcap_breakloop" HAVE_PCAP_BREAKLOOP )
+  check_function_exists( "pcap_create" HAVE_PCAP_CREATE )
+  if( HAVE_PCAP_CREATE OR WIN32 )
+    #
+    # If we have pcap_create(), we have pcap_set_buffer_size(), and
+    # can set the capture buffer size.
+    #
+    # Otherwise, if this is Windows, we have pcap_setbuff(), and can
+    # set the capture buffer size.
+    #
+    set( CAN_SET_CAPTURE_BUFFER_SIZE TRUE )
+  endif()
+  check_function_exists( "pcap_datalink_name_to_val" HAVE_PCAP_DATALINK_NAME_TO_VAL )
+  check_function_exists( "pcap_datalink_val_to_description" HAVE_PCAP_DATALINK_VAL_TO_DESCRIPTION )
+  check_function_exists( "pcap_datalink_val_to_name" HAVE_PCAP_DATALINK_VAL_TO_NAME )
+  check_function_exists( "pcap_findalldevs" HAVE_PCAP_FINDALLDEVS )
+  check_function_exists( "pcap_free_datalinks" HAVE_PCAP_FREE_DATALINKS )
+  check_function_exists( "pcap_get_selectable_fd" HAVE_PCAP_GET_SELECTABLE_FD )
+  check_function_exists( "pcap_lib_version" HAVE_PCAP_LIB_VERSION )
+  check_function_exists( "pcap_list_datalinks" HAVE_PCAP_LIST_DATALINKS )
+  check_function_exists( "pcap_set_datalink" HAVE_PCAP_SET_DATALINK )
+  check_function_exists( "bpf_image" HAVE_BPF_IMAGE )
+  check_function_exists( "pcap_setsampling" HAVE_PCAP_SETSAMPLING )
+  check_function_exists( "pcap_set_tstamp_precision" HAVE_PCAP_SET_TSTAMP_PRECISION )
+  # Remote pcap checks
+  check_function_exists( "pcap_open" HAVE_PCAP_OPEN )
+  if( HAVE_PCAP_OPEN )
+    set( HAVE_PCAP_REMOTE 1 )
+  endif()
+
+  cmake_pop_check_state()
+endif()
+
+mark_as_advanced( PCAP_LIBRARIES PCAP_INCLUDE_DIRS )
